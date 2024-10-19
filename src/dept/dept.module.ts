73657b1b303d4110615492db003c2dc12df0a65e
Ahmad-Softwaree <dr.ahmad.salah.54@gmail.com>
2024-10-20 00:01:02 +0300

    fix error

--- src/dept/dept.module.ts
+++ src/dept/dept.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { DeptService } from './dept.service';
 import { DeptController } from './dept.controller';
+import { PartGuard } from 'src/auth/part.guard';
+import { Reflector } from '@nestjs/core';
+import { KnexModule } from 'src/knex/knex.module';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   controllers: [DeptController],
-  providers: [DeptService],
+  providers: [DeptService, PartGuard, Reflector],
+  imports: [KnexModule, UserModule],
 })
 export class DeptModule {}
